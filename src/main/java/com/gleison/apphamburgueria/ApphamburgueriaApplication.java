a979a256ced9238146bef950c23a1b119690649f
Gleison Mozart <mozart.gleison@gmail.com>
2020-12-03 00:42:01 -0300

    Projeto Inicializado!

---
+++ src/main/java/com/gleison/apphamburgueria/ApphamburgueriaApplication.java
@@ -0,0 +1,13 @@
+package com.gleison.apphamburgueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApphamburgueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApphamburgueriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gleison/apphamburgueria/ApphamburgueriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleison.apphamburgueria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApphamburgueriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
