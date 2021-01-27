552a040b5f6efc6e261f4cb46e1129f62cbb7466
Gleison Mozart <mozart.gleison@gmail.com>
2021-01-27 01:10:39 -0300

    Criado os verbos http e seus tratamentos

--- src/main/java/com/gleison/apphamburgueria/controller/CategoriaController.java
+++ src/main/java/com/gleison/apphamburgueria/controller/CategoriaController.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,16 +23,28 @@ public class CategoriaController {
     private CategoriaRepository categoriaRepository;
 
     @GetMapping
-    public List<Categoria> listar(){
+    public ResponseEntity<?> listar(){
 
-      return categoriaRepository.findAll();
+        List<Categoria> categorias = categoriaRepository.findAll();
+
+      return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
     }
 
     @RequestMapping(value="/{id}", method = RequestMethod.GET)
     public Categoria find(@PathVariable Long id){
      Optional<Categoria> obj = categoriaRepository.findById(id);
      return obj.orElse(null);
+    }
 
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+        .buildAndExpand(categoriaSalva.getId()).toUri();
+        response.setHeader("Location",uri.toASCIIString());
+
+      return ResponseEntity.created(uri).body(categoriaSalva);
     }
 
 
