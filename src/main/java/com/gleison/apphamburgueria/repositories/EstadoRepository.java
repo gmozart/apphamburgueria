1d0f91bc0300ced9dc765584b58edefce039c421
Gleison Mozart <mozart.gleison@gmail.com>
2021-02-06 06:22:50 -0300

    Criada a classe Interface de Estado e Cidade

---
+++ src/main/java/com/gleison/apphamburgueria/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.gleison.apphamburgueria.repositories;
+
+import com.gleison.apphamburgueria.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+}
