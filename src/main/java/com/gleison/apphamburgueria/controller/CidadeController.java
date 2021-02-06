8395ef8f93853849b82f3cbec6b4c0bfce891370
Gleison Mozart <mozart.gleison@gmail.com>
2021-02-06 06:32:05 -0300

    Criado metodo de listar Cidades

--- src/main/java/com/gleison/apphamburgueria/controller/CidadeController.java
+++ src/main/java/com/gleison/apphamburgueria/controller/CidadeController.java
@@ -1,11 +1,28 @@
 package com.gleison.apphamburgueria.controller;
 
+import com.gleison.apphamburgueria.domain.Cidade;
+import com.gleison.apphamburgueria.repositories.CidadeRepository;
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cidades")
 public class CidadeController {
 
+  @Autowired
+  private CidadeRepository cidadeRepository;
+
+  public ResponseEntity<?> listar(){
+
+      List<Cidade> cidades = cidadeRepository.findAll();
+
+     return !cidades.isEmpty() ? ResponseEntity.ok(cidades) : ResponseEntity.noContent().build();
+  }
 
 }
