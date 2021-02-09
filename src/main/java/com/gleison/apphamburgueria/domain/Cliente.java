3865a591beca2e56a2c864a70b2f6aed5aa2827c
Gleison Mozart <mozart.gleison@gmail.com>
2021-02-08 23:13:18 -0300

    Criada a Classe Cliente e seus atributos

---
+++ src/main/java/com/gleison/apphamburgueria/domain/Cliente.java
@@ -0,0 +1,29 @@
+package com.gleison.apphamburgueria.domain;
+
+import com.gleison.apphamburgueria.domain.enums.TipoCliente;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CLIENTE")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "ID_CLIENTE")
+    private Long id;
+
+    @Column(name = "NOME_CLIENTE")
+    private String nome;
+
+    @Column(name = "EMAIL_CLIENTE")
+    private String email;
+
+    @Column(name = "CPF_CNPJ_CLIENTE")
+    private String cpfOUcnpj;
+
+    private TipoCliente tipo;
+
+
+
+}
