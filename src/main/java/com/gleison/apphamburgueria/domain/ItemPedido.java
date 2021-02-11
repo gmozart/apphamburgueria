01ada25ad582b03a0ad8f431df8e720a67d36b58
Gleison Mozart <mozart.gleison@gmail.com>
2021-02-11 07:02:04 -0300

    Finalizada a classe auxiliar ItemPedidoPK

--- src/main/java/com/gleison/apphamburgueria/domain/ItemPedido.java
+++ src/main/java/com/gleison/apphamburgueria/domain/ItemPedido.java
@@ -2,6 +2,8 @@ package com.gleison.apphamburgueria.domain;
 
 public class ItemPedido {
 
+    public ItemPedidoPK id = new ItemPedidoPK();
+
     private Double desconto;
 
     private Integer quantidade;
